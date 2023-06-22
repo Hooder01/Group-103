6de5f6ca500148a955de69dc35f94bdec7d53ef1
MitchellWillis05 <mitchellwillis33@gmail.com>
2023-06-22 19:55:43 +1200

    login system with files

--- New Source/Lunch/Lunch.cpp
+++ New Source/Lunch/Lunch.cpp
@@ -4,9 +4,57 @@
 
 using namespace std;
 
+void menu();
+
+void login();
+
+void signup();
+
+void read(string inputUsername, string inputPassword);
+
+bool logged;
+
+string user;
 
 int main()
 {
+    menu();
+
+    //return
+    return 0;
+}
+
+void menu()
+{
+    system("CLS");
+    int opt;
+    if (logged)
+    {
+        cout << "\tSchool Lunch Menu\tHello, " << user <<"\n1. order\n\n >> ";
+        cin >> opt;
+    }
+    else
+    {
+        cout << "\tSchool Lunch Menu\n1.login\t\t2.sign-up\t3.guest\n\n>> ";
+        cin >> opt;
+        switch (opt)
+        {
+        case 1:
+            login();
+            break;
+        case 2:
+            signup();
+            break;
+        }
+    }
+    
+    
+ 
+}
+
+void signup()
+{
+    system("CLS");
     //clear
     bool clear = false;
     //declare
@@ -23,9 +71,15 @@ int main()
     cout << "\n\nenter password\n>> ";
     cin >> password;
     //write
-    fout << "[" << username <<"|" << password << "]";
+    fout << "[" << username << "|" << password << "]";
     //close/commit
     fout.close();
+    //clear
+    if (clear == true)
+    {
+        clr.open("user_data.txt", ofstream::out | ofstream::trunc);
+        clr.close();
+    }
     //read
     fin.open("user_data.txt");
     while (!fin.eof())
@@ -35,12 +89,91 @@ int main()
     }
     //close
     fin.close();
-    if (clear == true)
-    {
-        clr.open("user_data.txt", std::ofstream::out | std::ofstream::trunc);
-        clr.close();
-    }
-    
-    //return
-    return 0;
+    cout << endl;
+    system("pause");
+    system("CLS");
+    cout << "successfully signed up\n";
+    system("pause");
+    menu();
 }
+
+void login()
+{
+    system("CLS");
+    string username, password;
+    cout << "enter username\n>> ";
+    cin >> username;
+    cout << "\n\nenter password\n>> ";
+    cin >> password;
+    read(username, password);
+}
+
+void read(string inputUsername, string inputPassword )
+{
+    bool flag = false;
+   
+    string data, load, build = "";
+    fstream fout;
+    ifstream fin;
+    fin.open("user_data.txt");
+    while (!fin.eof())
+    {
+        fin >> data;
+        load += data;
+    }
+    //detect
+    for (int i = 0; i < load.length(); ++i)
+    {
+        if (load[i] == '[')
+        {
+           
+            ++i;
+            while (load[i] != '|')
+            {
+                build += load[i];
+                ++i;
+            }
+            if (build == inputUsername)
+            {
+                user = build;
+                build = "";
+                ++i;
+                while (load[i] != ']')
+                {
+                    build += load[i];
+                    ++i;
+                }
+                if (build == inputPassword)
+                {
+                    flag = true;
+                    logged = true;
+                    build = "";
+                    break;
+                }
+                else
+                {
+                    build = "";
+                }
+            }
+            else
+            {
+                build = "";
+            }
+        }
+        ++i;
+    }
+    if (flag)
+    {
+        cout << "\nsuccessfully logged in.";
+    }
+    else
+    {
+        cout << "username or password is incorrect, please try again.";
+    }
+    //close
+    fin.close();
+    cout << endl;
+    system("pause");
+    menu();
+}
+
